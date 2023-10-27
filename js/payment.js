8affb5ccdf76e71d5cdb214d0d966ea3153c9363
Manan Mandaliya <58840732+mananM27@users.noreply.github.com>
2023-10-27 10:00:28 +0530

    Update payment.js
    Updated new WhatsApp number in link

--- js/payment.js
+++ js/payment.js
@@ -102,7 +102,7 @@ function pay() {
     btn.style.color = "white";
     btn.addEventListener("click", () => {
       const string = encodeURIComponent(`🌟Hi Dharma, Unleash the Celebrity Magic on your special day🌟\n\nWant a Personalised Birthday Video Message from ${localStorage.getItem("celeb")}? 🌠✨\n\nGuess what? 🎉 Delivery in just 24 hours! ⏰🚀\n\nSo, why wait? Click below to get FLAT10% OFF 🌟🎥\n\n100% Refund Guarantee if video is not delivered on time! 🤝`);
-      window.open( `https://wa.me/916376657384?text=${string}`)
+      window.open( `https://wa.me/919024076090?text=${string}`)
       mainContainer.removeChild(modal);
     });
 
